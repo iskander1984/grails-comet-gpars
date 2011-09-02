798061cc0f843ddc1e644843c95a0ae0ed4bf01c
Pochapskiy Olexandr <olexandr.pochapskiy@gmail.com>
2011-09-02 18:27:16 +0300

    Added dependency to GPars 0.12

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,13 +23,13 @@ grails.project.dependency.resolution = {
         //mavenCentral()
         //mavenLocal()
         //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
+		build 'org.codehaus.gpars:gpars:0.12'
         // runtime 'mysql:mysql-connector-java:5.1.16'
     }
 
